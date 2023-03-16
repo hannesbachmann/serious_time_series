e21d700d56b73dd0f7b98639cc180fa62cf3999f
Hannes <hannes-bachmann@t-online.de>
2023-03-16 19:29:24 +0100

    handle measurement values between 2021-02-10T00:00:00 and 2021-02-17T00:00:00

--- clean_up_data/handle_outliers.py
+++ clean_up_data/handle_outliers.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def handle_bad_values(ts):
+def handle_to_high_values(ts):
     """P_pool_historical measurements between 2022-02-02T00:00:00 and 2022-02-02T23:45:00 show some bad behavior:
     The values are much too high.
     But dividing the power values in this timerange by 200 'fix' this issue quite well."""
@@ -12,4 +12,38 @@ def handle_bad_values(ts):
         lambda row: row['P_pool_historical'] if row['timestamp'] < pd.to_datetime('2022-02-02T00:00:00') or row[
             'timestamp'] > pd.to_datetime('2022-02-02T23:45:00') else row['P_pool_historical'] / 200, axis=1)
 
+    return ts
+
+
+def handle_shut_down_values(ts, replacing_method='zeros'):
+    """problematic measurement values spots
+    between 2021-02-10T00:00:00 and 2021-02-17T00:00:00
+    maybe caused by a shut-down.
+    values: replace with values from some weeks before
+    zeros: replace with zero values
+    delete: delete rows"""
+    if replacing_method == 'values':
+        dt_start = pd.to_datetime('2021-02-10T00:00:00') - pd.Timedelta(days=28)
+        dt_end = pd.to_datetime('2021-02-10T00:00:00') - pd.Timedelta(days=7)
+
+        ts1 = ts.set_index('timestamp')
+        week_before = ts1[dt_start:dt_end]
+        week_before = week_before.reset_index()
+        week_before['timestamp'] = week_before['timestamp'].apply(lambda date: date + pd.Timedelta(days=14))
+        week_before = week_before.set_index('timestamp')
+
+        ts['P_pool_historical'] = ts.apply(
+            lambda row: row['P_pool_historical'] if row['timestamp'] < pd.to_datetime('2021-02-10T00:00:00') - pd.Timedelta(days=7) or row[
+                'timestamp'] > pd.to_datetime('2021-02-10T00:00:00') + pd.Timedelta(days=7) else week_before['P_pool_historical'][row['timestamp']], axis=1)
+    elif replacing_method == 'zeros':
+        ts['P_pool_historical'] = ts.apply(
+            lambda row: row['P_pool_historical'] if row['timestamp'] < pd.to_datetime(
+                '2021-02-10T00:00:00') or row['timestamp'] > pd.to_datetime(
+                '2021-02-10T00:00:00') + pd.Timedelta(days=7) else 0,
+            axis=1)
+    elif replacing_method == 'delete':
+        ts = ts.set_index('timestamp')
+        ts_before = ts[:pd.to_datetime('2021-02-10T00:00:00')]
+        ts_after = ts[pd.to_datetime('2021-02-10T00:00:00') + pd.Timedelta(days=7):]
+        ts = ts_before.append(ts_after).reset_index()
     return ts
--- time_series_analysation/view_raw_data.py
+++ time_series_analysation/view_raw_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from time_series_analysation.data_loader import Loader
+from clean_up_data.handle_outliers import handle_to_high_values
+from clean_up_data.handle_outliers import handle_shut_down_values
 
 
 def plot_time_series(ts=pd.DataFrame({'timestamp': [], 'values': []})):
@@ -21,8 +23,24 @@ def plot_time_series(ts=pd.DataFrame({'timestamp': [], 'values': []})):
     pass
 
 
+def plot_time_series_yearly(ts):
+    # split timeseries into smaller yearly dataframe timeseries
+    years = [g for n, g in ts.set_index('timestamp').groupby(pd.Grouper(freq='Y'))]
+
+    for i_y in range(1, len(years)):
+        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+        ax1.plot(years[i_y-1].index, years[i_y-1]['P_pool_historical'])
+        ax2.plot(years[i_y].index, years[i_y]['P_pool_historical'])
+        plt.show()
+        pass
+    pass
+
+
 if __name__ == '__main__':
     L = Loader()
     pool, substations = L.get_data()
-    plot_time_series(ts=pool[['timestamp', 'P_pool_historical']])
+    pool = handle_to_high_values(ts=pool[['timestamp', 'P_pool_historical']])
+    pool = handle_shut_down_values(ts=pool, replacing_method='values')
+    plot_time_series_yearly(ts=pool)
+    plot_time_series(ts=pool)
     pass
