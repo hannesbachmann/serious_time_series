c668d9378b79378b4068efde2485acfe7ab4aaaf
Hannes <hannes-bachmann@t-online.de>
2023-03-27 10:39:52 +0200

    seasonality analysis

--- time_series_analysation/seasonality_analysis.py
+++ time_series_analysation/seasonality_analysis.py
@@ -1,4 +1,4 @@
-"""analysis of trend and saisonality"""
+"""analysis of trend and seasonality"""
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,16 +18,16 @@ def calc_linear_trend(ts):
     air_passengers_detrended = ts - trend_pred
 
     f, axes = plt.subplots(1, 2, figsize=(12, 4))
-    axes[0].set_title('%.2f mehr Passagiere pro Monat (linearer Trend)' % regr.coef_[0])
+    axes[0].set_title('%.2f more power (linear trend)' % regr.coef_[0])
     axes[0].plot(x, y)
     axes[0].plot(x, trend_pred, color='black')
-    axes[0].set_xlabel('Zeit $t$')
-    axes[0].set_ylabel('Passagiere')
+    axes[0].set_xlabel('time $t$')
+    axes[0].set_ylabel('power')
 
-    axes[1].set_title('Trendbereinigte Zahl der Passagiere')
+    axes[1].set_title('power without trend')
     axes[1].plot(x, air_passengers_detrended)
-    axes[1].set_xlabel('Zeit $t$')
-    axes[1].set_ylabel('Passagiere')
+    axes[1].set_xlabel('time $t$')
+    axes[1].set_ylabel('power')
     plt.show()
     # result: there is not really a trend over the years
     pass
@@ -35,27 +35,22 @@ def calc_linear_trend(ts):
 
 def calc_seasonality(ts):
     """filter seasonality"""
-    decomposed = seasonal_decompose(ts['P_pool_historical'], model='additive', period=4*24*7)
-    decomposed.plot()
+    # separate weekly trend
+    decomposed_week = seasonal_decompose(ts['P_pool_historical'], model='additive', period=4*24*7)
+    ts['seasonal_week'] = decomposed_week.seasonal
+    week_mean = decomposed_week.trend.mean() + decomposed_week.resid.mean()
+    ts['without_seasonal'] = ts['P_pool_historical'] - decomposed_week.seasonal     # = trend + resid for week
+    # separate yearly trend
+    decomposed_year = seasonal_decompose(ts['without_seasonal'], model='additive', period=4 * 24 * 365)
+    ts['seasonal_year'] = decomposed_year.seasonal
+    ts['without_seasonal_year'] = ts['without_seasonal'] - decomposed_year.seasonal     # = trend + resid for year
+    year_mean = decomposed_year.trend.mean() + decomposed_year.resid.mean()
+    df = ts[['P_pool_historical', 'without_seasonal_year']]
+    # composition evaluation using the seasonal effects for a week and a year and the means of trend and resid
+    df['composed'] = ts['seasonal_week'] + ts['seasonal_year'] + year_mean
+
+    decomposed_week.plot()
     plt.show()
-
-    ts['seasonal'] = decomposed.seasonal
-    ts['without_seasonal'] = ts['P_pool_historical'] - (decomposed.seasonal + 10000)
-
-    decomposed_year = seasonal_decompose(ts['without_seasonal'], model='additive', period=4*24*365)
-    ts['without_seasonal_year'] = ts['without_seasonal'] - decomposed_year.seasonal
-
-    # f, axes = plt.subplots(1, 2, figsize=(12, 4))
-    # axes[0].set_title('Monatliche Abweichung vom Mittelwert')
-    # axes[0].bar(['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'], monthly_means)
-    # axes[0].set_xlabel('Zeit $t$')
-    # axes[0].set_ylabel('Passagiere')
-    #
-    # axes[1].set_title('Trend- und saisonbereinigte Zahl der Passagiere')
-    # axes[1].plot(x, air_passengers_detrended_unseasonal)
-    # axes[1].set_xlabel('Zeit $t$')
-    # axes[1].set_ylabel('Passagiere')
-    # plt.show()
 
     pass
 
